d2bccf75419b89428a2342d785164ad1406b5b33
Sheba467 <lowyouone@gmail.com>
2021-03-03 12:28:47 +0200

    initialize material-ui dashboard

---
+++ src/components/deposits/deposits.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import { Title } from '../title/title';
+interface depositsProps { }
+const useStyles = makeStyles({
+  depositContext: {
+    flex: 1,
+  },
+});
+export const Deposits: React.FC<depositsProps> = () => {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <Title>Recent Deposits</Title>
+      <Typography component="p" variant="h4">
+        $3,024.00
+        </Typography>
+      <Typography color="textSecondary" className={classes.depositContext}>
+        on 15 March, 2019
+        </Typography>
+    </React.Fragment>
+  );
+}
